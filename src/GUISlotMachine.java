d5d3e754d300a2ca630b3ec179516245b71b3831
Tyler Reinecke <Guest@wts8.wts.local>
2015-10-04 19:00:43 -0700

    10/4/15

---
+++ src/GUISlotMachine.java
@@ -0,0 +1,82 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+public class GUISlotMachine implements ActionListener
+{
+	JFrame frame;
+	JPanel panel;
+	JButton spin;
+	JTextField bet;
+	JLabel one;
+	JLabel two;
+	JLabel three;
+	JLabel betValue;
+	JLabel totalValue;
+	
+	int totalCash = 1000;
+	int betCash = 0;
+	
+	public static void main(String[] args) {
+		GUISlotMachine r = new GUISlotMachine();
+	}
+	
+	public GUISlotMachine()
+	{
+		//GUI
+		frame = new JFrame();
+		panel = new JPanel();
+		frame.add(panel);
+		frame.setSize(500, 500);
+		frame.setLayout(null);
+		
+		panel.setBounds(0, 0, 500, 500);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		
+		betValue = new JLabel("Bet:");
+		panel.add(betValue);
+		betValue.setBounds(10, 10, 30, 20);
+		
+		bet = new JTextField(10);
+		panel.add(bet);
+		bet.setBounds(50, 10, 30, 20);
+		
+		spin = new JButton("Spin");
+		panel.add(spin);
+		spin.setBounds(175, 200, 75, 75);
+		spin.addActionListener(this);
+		
+		one = new JLabel("0");
+		panel.add(one);
+		one.setBounds(50, 350, 150, 100);
+		
+		two = new JLabel("0");
+		panel.add(two);
+		two.setBounds(200, 350, 150, 100);
+		
+		three = new JLabel("0");
+		panel.add(three);
+		three.setBounds(350, 350, 150, 100);
+
+		totalValue = new JLabel("Cash: 1000");
+		panel.add(totalValue);
+		totalValue.setBounds(20, 150, 200, 40);
+		//totalValue.setText("Cash: " + totalCash);
+	}
+	
+	public void actionPerformed(ActionEvent e)
+	{
+		if(e.getSource()==spin)
+		{
+			betCash = Integer.parseInt(bet.getText());
+		}
+	}
+}
